74e645d02c843b77cf2b60e935fdaa09520a60a4
wgall <wgall@GALLDESKT.localdomain>
2022-12-11 18:59:52 +0100

    Final CICD

--- .github/actions/Deployment/deployment_files.sh
+++ .github/actions/Deployment/deployment_files.sh
@@ -5,35 +5,35 @@ cat <<EOF > /files/argocd-deployments/$1/stage/deployment.yaml
 apiVersion: v1
 kind: Service
 metadata:
-  name: nextapp-service
+  name: $1-service
 spec:
   ports:
     - name: http
       port: 80
       targetPort: 3000
   selector:
-    app: nextapp
+    app: $1
   type: NodePort
 ---
 apiVersion: apps/v1
 kind: Deployment
 metadata:
   labels:
-    app: nextapp
-  name: nextapp
+    app: $1
+  name: $1
 spec:
   selector:
     matchLabels:
-      app: nextapp
+      app: $1
   template:
     metadata:
       labels:
-        app: nextapp
+        app: $1
     spec:
       containers:
         -
           image: gcr.io/$PROJECTID/$IMAGE
-          name: nextapp
+          name: $1
           ports:
             -
               containerPort: 3000
